a1d7cd3432093e893261a00a07c25d6b1b5a6c8b
Erik Tellier <erik.tellier.etps@gmail.com>
2023-10-15 14:44:54 +0200

    Added CRUD for CATEGORIE_ABONNE

---
+++ db/CATEGORIE_ABONNE.go
@@ -0,0 +1,38 @@
+package db
+
+import "database/sql"
+
+func CreateCategorieAbonne(db *sql.DB, idCategorie string) error {
+	_, err := db.Exec("INSERT INTO CATEGORIE_ABONNE (ID_CATEGORIE) VALUES (?)", idCategorie)
+	return err
+}
+
+func ReadCategorieAbonne(db *sql.DB) ([]string, error) {
+	rows, err := db.Query("SELECT ID_CATEGORIE FROM CATEGORIE_ABONNE")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var categories []string
+
+	for rows.Next() {
+		var category string
+		if err := rows.Scan(&category); err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
+}
+
+func UpdateCategorieAbonne(db *sql.DB, idCategorie string, newIdCategorie string) error {
+	_, err := db.Exec("UPDATE CATEGORIE_ABONNE SET ID_CATEGORIE = ? WHERE ID_CATEGORIE = ?", newIdCategorie, idCategorie)
+	return err
+}
+
+func DeleteCategorieAbonne(db *sql.DB, idCategorie string) error {
+	_, err := db.Exec("DELETE FROM CATEGORIE_ABONNE WHERE ID_CATEGORIE = ?", idCategorie)
+	return err
+}
